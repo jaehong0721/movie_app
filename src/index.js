40136923c07ac3c7079045caf1289e044bbd454e
jaehong <newbie302@gmail.com>
2018-08-30 17:56:46 +0900

    ReactDOM의 render 함수 주석 추가

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+//App컴포넌트를 index.html의 id가 root인 element에 출력(render)
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
